7441ef5dd57fd0ec62f1de5a45752f8a833582bc
Tarys <tarys@foxmail.com>
2020-12-05 00:21:22 -0800

    Init

---
+++ domain/include/entity.h
@@ -0,0 +1,11 @@
+#ifndef ENTITY_H_1204
+#define ENTITY_H_1204
+
+typedef struct Poker
+{
+    unsigned char pointValue;
+    unsigned char color;
+    unsigned char type;
+}Poker;
+
+#endif
---
+++ infra/pub.h
@@ -0,0 +1,6 @@
+#ifndef PUB_H_12204
+#define PUB_H_12204
+
+
+
+#endif
